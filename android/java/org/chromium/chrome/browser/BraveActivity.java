e524a288f140098ee42e661f1219d8fd8a83196b
samartnik <artem@brave.com>
2020-06-15 08:48:41 -0400

    [Android] Moved disabling NTP suggestions on earlier stage In Chromium 84 they execute code based on these values before we disable them

--- android/java/org/chromium/chrome/browser/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/BraveActivity.java
@@ -102,10 +102,6 @@ public abstract class BraveActivity<C extends ChromeActivityComponent> extends C
     @Override
     public void onStartWithNative() {
         super.onStartWithNative();
-
-        // Disable NTP suggestions
-        PrefServiceBridge.getInstance().setBoolean(Pref.NTP_ARTICLES_SECTION_ENABLED, false);
-        PrefServiceBridge.getInstance().setBoolean(Pref.NTP_ARTICLES_LIST_VISIBLE, false);
     }
 
     @Override
