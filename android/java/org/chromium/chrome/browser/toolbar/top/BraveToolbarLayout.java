ebdb1b35143c2cc59b4ff7f96d2108276017ff7a
samartnik <artem@brave.com>
2020-07-15 16:00:38 -0400

    [Android] Transition animation has been changed
    This affects our previous fix 'Fixed transition animation for top toolbar'
    https://github.com/brave/brave-core/commit/49de25ca13644e3bd3186ac4262bef2739611c10
    
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/79638f651a8f027a48c2cbb0732c25ddcab8e47b
    
    [ToolbarMVC] Fix MenuButton drawing during TabSwitcher animation
    
    This change has MenuButton control its tab switcher animation drawing internally so that ToolbarPhone doesn't need to keep track of which Drawable to use. It also adds some regression tests.
    
    Bug: 1082771

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayout.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayout.java
@@ -184,8 +184,8 @@ public abstract class BraveToolbarLayout extends ToolbarLayout implements OnClic
     // shown and loading state is changed.
     updateBraveShieldsButtonState(null);
     if (this instanceof ToolbarPhone) {
-      if (getMenuButtonWrapper() != null && BottomToolbarVariationManager.isMenuButtonOnBottom()) {
-        getMenuButtonWrapper().setVisibility(View.GONE);
+      if (super.getMenuButtonWrapper() != null && BottomToolbarVariationManager.isMenuButtonOnBottom()) {
+        super.getMenuButtonWrapper().setVisibility(View.GONE);
       }
     }
   }
@@ -721,19 +721,9 @@ public abstract class BraveToolbarLayout extends ToolbarLayout implements OnClic
   }
 
   @Override
-  boolean isShowingAppMenuUpdateBadge() {
-    if (this instanceof ToolbarPhone &&
-        BottomToolbarVariationManager.isMenuButtonOnBottom()) {
-      // Just to avoid drawing menu button on transition animation
-      return true;
-    }
-    return super.isShowingAppMenuUpdateBadge();
-  }
-
-  @Override
   public void onBottomToolbarVisibilityChanged(boolean isVisible) {
-    if (this instanceof ToolbarPhone && getMenuButtonWrapper() != null) {
-      getMenuButtonWrapper().setVisibility(isVisible ? View.GONE : View.VISIBLE);
+    if (this instanceof ToolbarPhone && super.getMenuButtonWrapper() != null) {
+      super.getMenuButtonWrapper().setVisibility(isVisible ? View.GONE : View.VISIBLE);
     }
   }
 
@@ -750,4 +740,11 @@ public abstract class BraveToolbarLayout extends ToolbarLayout implements OnClic
     }
     updateModernLocationBarColor(mCurrentToolbarColor);
   }
+
+    View getMenuButtonWrapper() {
+        if (this instanceof ToolbarPhone && BottomToolbarVariationManager.isMenuButtonOnBottom()) {
+            return null;
+        }
+        return super.getMenuButtonWrapper();
+    }
 }
