76a1b7ee03c9b97fb40d70e89d1f0f1a40ab2313
samartnik <artem@brave.com>
2021-09-07 10:13:10 -0400

    [Android] MobileIdentityConsistency was removed
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/c41aaf4b383dbe02f289ad0cd1be5cc016d238f9
    
    Remove references to kMobileIdentityConsistency experiment in Chrome.
    
    Following launch https://crbug.com/1159797, remove references to
    experimental code across Android and iOS platforms.
    
    Fixed: 1241417

--- app/brave_main_delegate.cc
+++ app/brave_main_delegate.cc
@@ -235,7 +235,6 @@ bool BraveMainDelegate::BasicStartupComplete(int* exit_code) {
     feed::kInterestFeedContentSuggestions.name,
     feed::kInterestFeedV2.name,
     offline_pages::kPrefetchingOfflinePagesFeature.name,
-    signin::kMobileIdentityConsistency.name,
     translate::kTranslate.name,
 #endif
   };
