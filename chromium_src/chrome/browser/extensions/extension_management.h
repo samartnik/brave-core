932d467707bb0b0209d8c078690c52f1be17a72b
Claudio DeSouza <cdesouza@igalia.com>
2022-04-20 14:19:10 +0100

    Adding override files for ExtensionService/ExtensionManagement
    This is an update to the more recent approach of how classes should be
    made private friends.

---
+++ chromium_src/chrome/browser/extensions/extension_management.h
@@ -0,0 +1,17 @@
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_EXTENSIONS_EXTENSION_MANAGEMENT_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_EXTENSIONS_EXTENSION_MANAGEMENT_H_
+
+#define BRAVE_EXTENSION_MANAGEMENT_H \
+ private:                            \
+  friend class BraveExtensionManagement;
+
+#include "src/chrome/browser/extensions/extension_management.h"
+
+#undef BRAVE_EXTENSION_MANAGEMENT_H
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_EXTENSIONS_EXTENSION_MANAGEMENT_H_
