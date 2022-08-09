d9568fea6c6ad4f7b2d2fc136bef8da5ef38a8b4
Claudio DeSouza <cdesouza@igalia.com>
2022-08-09 14:10:03 +0100

    fixup! PrefService::GetDictValue returns a reference

--- components/brave_ads/browser/ads_service_impl.cc
+++ components/brave_ads/browser/ads_service_impl.cc
@@ -1792,12 +1792,7 @@ void AdsServiceImpl::SetTimePref(const std::string& path,
 
 absl::optional<base::Value::Dict> AdsServiceImpl::GetDictPref(
     const std::string& path) const {
-  const base::Value::Dict* value = profile_->GetPrefs()->GetValueDict(path);
-  if (!value) {
-    return absl::nullopt;
-  }
-
-  return value->Clone();
+  return profile_->GetPrefs()->GetValueDict(path).Clone();
 }
 
 void AdsServiceImpl::SetDictPref(const std::string& path,
@@ -1808,12 +1803,7 @@ void AdsServiceImpl::SetDictPref(const std::string& path,
 
 absl::optional<base::Value::List> AdsServiceImpl::GetListPref(
     const std::string& path) const {
-  const base::Value::List* value = profile_->GetPrefs()->GetValueList(path);
-  if (!value) {
-    return absl::nullopt;
-  }
-
-  return value->Clone();
+  return profile_->GetPrefs()->GetValueList(path).Clone();
 }
 
 void AdsServiceImpl::SetListPref(const std::string& path,
