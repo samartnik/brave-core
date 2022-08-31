05b7d4c8e64a53d02b897181a78de777d4f56b1d
Claudio DeSouza <cdesouza@igalia.com>
2022-08-31 15:27:43 +0100

    BookmarkModelFactory/ManagedBookmarkServiceFactory browser context fixes
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/2133889a351ad2ed8bf961617ddd90f2ee4766d8
    
    commit 2133889a351ad2ed8bf961617ddd90f2ee4766d8
    Author: Ryan Sultanem <rsult@google.com>
    Date:   Fri Aug 12 12:13:10 2022 +0000
    
        Migration of chrome/ BrowserContextKeyedServiceFactory to ProfileKeyedServiceFactory Part 5
    
        Bug: 1284664

--- chromium_src/chrome/browser/bookmarks/bookmark_model_factory.h
+++ chromium_src/chrome/browser/bookmarks/bookmark_model_factory.h
@@ -3,7 +3,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef aaa
-#define aaa
+#ifndef BRAVE_CHROMIUM_SRC_CHROME_BROWSER_BOOKMARKS_BOOKMARK_MODEL_FACTORY_H_
+#define BRAVE_CHROMIUM_SRC_CHROME_BROWSER_BOOKMARKS_BOOKMARK_MODEL_FACTORY_H_
 
-#endif  // AAA
+#include "chrome/browser/profiles/profile_keyed_service_factory.h"
+
+#define ServiceIsNULLWhileTesting                       \
+  ServiceIsNULLWhileTesting_unused();                   \
+  content::BrowserContext* GetBrowserContextToUse(      \
+      content::BrowserContext* context) const override; \
+  bool ServiceIsNULLWhileTesting
+
+#include "src/chrome/browser/bookmarks/bookmark_model_factory.h"
+
+#undef ServiceIsNULLWhileTesting
+
+#endif  // BRAVE_CHROMIUM_SRC_CHROME_BROWSER_BOOKMARKS_BOOKMARK_MODEL_FACTORY_H_
