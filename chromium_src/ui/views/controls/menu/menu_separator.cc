998d4a7c21df53a840a61e618a1bfde8fe0f1282
Claudio DeSouza <cdesouza@igalia.com>
2023-04-11 12:33:48 +0100

    Reanchoring MenuSeparator overrides
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/5f363abef4eb1fd8f8ccac5554e7cd27799754bf
    
    commit 5f363abef4eb1fd8f8ccac5554e7cd27799754bf
    Author: Allen Bauer <kylixrd@chromium.org>
    Date:   Thu Apr 6 15:34:18 2023 +0000
    
        [CR2023] Update 3-dot Zoom menu item, separators, Find sub-menu.
    
        https://screenshot.googleplex.com/72BUn2MFczp452Y.png
        https://screenshot.googleplex.com/9tX3VZwy8gjGnEd.png
    
        Bug: 1427667

--- chromium_src/ui/views/controls/menu/menu_separator.cc
+++ chromium_src/ui/views/controls/menu/menu_separator.cc
@@ -3,9 +3,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#define BRAVE_MENU_SEPARATOR_ON_PAINT               \
-  else if (type_ == ui::BOTH_SIDE_PADDED_SEPARATOR) \
-      paint_rect.Inset(gfx::Insets::VH(0, 8));
+#define BRAVE_MENU_SEPARATOR_ON_PAINT                 \
+  else if (type_ == ui::BOTH_SIDE_PADDED_SEPARATOR) { \
+    paint_rect.Inset(gfx::Insets::VH(0, 8));          \
+  }
 
 #include "src/ui/views/controls/menu/menu_separator.cc"
 
