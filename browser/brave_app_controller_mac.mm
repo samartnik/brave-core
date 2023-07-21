eccd437bb23f59d16d34a9cff773d542fdced514
Claudio DeSouza <cdesouza@brave.com>
2023-07-21 05:21:36 +0100

    fixup! Overriding `AppController.sharedController`

--- browser/brave_app_controller_mac.mm
+++ browser/brave_app_controller_mac.mm
@@ -18,6 +18,10 @@
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_finder.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 @implementation BraveAppController
 
 @synthesize copyMenuItem = _copyMenuItem;
