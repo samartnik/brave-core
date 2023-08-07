bc2de00afe2e8c5fa904b67da837ec5f4123017f
Artem Samoilenko <artem@brave.com>
2023-08-07 12:26:48 -0400

    [Android] Explicitly added action for Brave intents
    Otherwise there is lint issue:
    Warning: This intent has no action set and is not explicit by component.You should either make this intent explicit by component or set an action matching the targeted intent filter. [IntentWithNullActionLaunch]

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -1346,6 +1346,7 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
                     && OnboardingPrefManager.getInstance().getUrlFocusCount() == 1
                     && !mBraveSearchEngineDefaultRegions.contains(countryCode)) {
                 Intent searchActivityIntent = new Intent(context, SearchActivity.class);
+                searchActivityIntent.setAction(Intent.ACTION_VIEW);
                 context.startActivity(searchActivityIntent);
             }
 
