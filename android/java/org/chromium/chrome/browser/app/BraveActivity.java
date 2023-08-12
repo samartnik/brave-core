24dc7e10f5022286277cf25f9b2534e5f48b718d
Max Karolinskiy <max@brave.com>
2023-08-11 21:06:27 -0400

    [Android] integerListToIntArray -> integerCollectionToIntArray.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/9ccf06ad6b9d9dbf887274f60665bba8af145e4c
    
    commit 9ccf06ad6b9d9dbf887274f60665bba8af145e4c
    Author: vikas.mundra <vikas.mundra@samsung.com>
    Date:   Tue Aug 8 14:45:01 2023 +0000
    
        Change integerListToIntArray to handle Collection<Integer>.
    
        Bug: None

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -184,7 +184,6 @@ import org.chromium.mojo.bindings.ConnectionErrorHandler;
 import org.chromium.mojo.system.MojoException;
 import org.chromium.ui.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
@@ -738,7 +737,7 @@ public abstract class BraveActivity extends ChromeActivity
             List<Integer> dataTypes = Arrays.asList(
                     BrowsingDataType.HISTORY, BrowsingDataType.COOKIES, BrowsingDataType.CACHE);
 
-            int[] dataTypesArray = CollectionUtil.integerListToIntArray(new ArrayList<>(dataTypes));
+            int[] dataTypesArray = CollectionUtil.integerCollectionToIntArray(dataTypes);
 
             // has onBrowsingDataCleared() as an @Override callback from implementing
             // BrowsingDataBridge.OnClearBrowsingDataListener
