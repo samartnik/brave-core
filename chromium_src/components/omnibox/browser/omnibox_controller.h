0bd984f36f88be52b7a42b29e4a7d37742eb8e76
mkarolin <max@brave.com>
2023-08-20 18:52:00 -0400

    Fixes accidental virtualization of method in OmniboxEditMode.
    The below Chromium change added inclusion of OminiboxEditModel header
    file into OmniboxController header where we virtualize StartAutocomplete
    method. However, OmniboxEditModel also has StartAutocomplete method that
    we started virtualizing only for this inclusion which created bad
    vtable.
    
    Also, fixes an override for autocomplete_controller.h which was
    redefining a symbol that was used multiple times in the original file.
    
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/4d989869470674674152634980ff68257c34febb
    
    commit 4d989869470674674152634980ff68257c34febb
    Author: Moe Ahmadi <mahmadi@chromium.org>
    Date:   Wed Jun 21 12:12:21 2023 +0000
    
        [omnibox] Removes accessors to unowned instances in OmniboxEditModel
    
        This change addresses some long standing TODOs by de-spaghettifying
        the Omnibox UI classes and making the structure more tenable by
        removing accessors to the ACController, ACResult, and OmniboxClient in
        the OmniboxEditModel.
    
        These instances are owned by OmniboxController. Since OmniboxController
        owns the OmniboxEditModel, any existing or future consumer of those
        instances should technically be able to access them directly via
        OmniboxController, if they are able to access them via OmniboxEditModel.
    
        Also rather than passing OmniboxEditModel to OmniboxResultView,
        OmniboxRowView, and OmniboxSuggestionButtonRowView, this change also
        adds convenience accessors for OmniboxController and OmniboxEditModel
        to OmniboxPopupView base class to be used in its implementations and
        their child views.
    
        Low-Coverage-Reason: making a mechanical change.
    
        Bug: 1404748

--- chromium_src/components/omnibox/browser/omnibox_controller.h
+++ chromium_src/components/omnibox/browser/omnibox_controller.h
@@ -1,16 +1,21 @@
 /* Copyright (c) 2019 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_CONTROLLER_H_
 #define BRAVE_CHROMIUM_SRC_COMPONENTS_OMNIBOX_BROWSER_OMNIBOX_CONTROLLER_H_
 
+// Also has StartAutocomplete member
+#include "components/omnibox/browser/omnibox_edit_model.h"
+
 #define StartAutocomplete virtual StartAutocomplete
+
 #define SetRichSuggestionBitmap        \
   SetRichSuggestionBitmapUnused();     \
   friend class BraveOmniboxController; \
   void SetRichSuggestionBitmap
+
 #include "src/components/omnibox/browser/omnibox_controller.h"  // IWYU pragma: export
 #undef StartAutocomplete
 #undef SetRichSuggestionBitmap
