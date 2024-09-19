b92faecc96b5601ca20f565d11b4798ff116eed2
Emerick Rogul <erogul@brave.com>
2024-09-19 16:46:28 -0400

    Revert "[WIP] Download Manager item override export fixed"
    This reverts commit 013423598f38169c1715fd468e2f50ab0ccf357d.

--- browser/resources/downloads/brave_overrides/item.ts
+++ browser/resources/downloads/brave_overrides/item.ts
@@ -3,16 +3,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// import type {DownloadsItemElement} from './item.js';
+import { ItemElement } from '../item.js'
 
-// import { injectStyle } from '//resources/brave/lit_overriding.js'
-// import { css } from '//resources/lit/v3_0/lit.rollup.js'
+import { injectStyle } from '//resources/brave/lit_overriding.js'
+import { css } from '//resources/lit/v3_0/lit.rollup.js'
 
-// injectStyle(
-//   DownloadsItemElement,
-//   css`
-//       #progress {
-//         --cr-progress-active-color: var(--leo-color-icon-interactive) !important;
-//       }
-//   `
-// )
+injectStyle(
+  ItemElement,
+  css`
+      #progress {
+        --cr-progress-active-color: var(--leo-color-icon-interactive) !important;
+      }
+  `
+)
