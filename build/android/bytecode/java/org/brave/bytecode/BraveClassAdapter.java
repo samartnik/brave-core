c7bc0751caaf4be214d2987907f61bb7c1dfb410
AlexeyBarabash <alexey@brave.com>
2024-09-20 11:33:39 +0300

    [WIP] [Android] Disabled bytecode patch around BraveDefaultBrowserPromoUtils, it caused crash
    With the commit "[Android] DefaultBrowserPromoUtils.prepareLaunchPromoIfNeeded changed"
    
    debug app crashes at the start with an error
    `java.lang.VerifyError: Rejecting class org.chromium.chrome.browser.tabbed_mode.BraveTabbedRootUiCoordinator that attempts to sub-type erroneous class`
    
    attempt to build release app fails with error
    ```
    Missing class org.chromium.chrome.browser.ui.default_browser_promo.BraveDefaultBrowserPromoUtils (referenced from: boolean org.chromium.chrome.browser.tabbed_mode.l.L(org.chromium.chrome.browser.profiles.Profile))
    Missing method boolean org.chromium.chrome.browser.ui.default_browser_promo.BraveDefaultBrowserPromoUtils.prepareLaunchPromoIfNeeded(android.app.Activity, org.chromium.ui.base.WindowAndroid, org.chromium.components.feature_engagement.Tracker, boolean) (referenced from: boolean org.chromium.chrome.browser.tabbed_mode.l.L(org.chromium.chrome.browser.profiles.Profile))
    ```
    
    This commit disables BraveDefaultBrowserPromoUtils patch to make the app be able to launch.

--- build/android/bytecode/java/org/brave/bytecode/BraveClassAdapter.java
+++ build/android/bytecode/java/org/brave/bytecode/BraveClassAdapter.java
@@ -38,7 +38,9 @@ public class BraveClassAdapter {
         chain = new BraveContentSettingsResourcesClassAdapter(chain);
         chain = new BraveContentViewClassAdapter(chain);
         chain = new BraveCustomizationProviderDelegateImplClassAdapter(chain);
-        chain = new BraveDefaultBrowserPromoUtilsClassAdapter(chain);
+        // TODO(alexeybarabash): BraveDefaultBrowserPromoUtils now causes crash on debug build at
+        // start app
+        // chain = new BraveDefaultBrowserPromoUtilsClassAdapter(chain);
         chain = new BraveDownloadMessageUiControllerImplClassAdapter(chain);
         chain = new BraveDropdownItemViewInfoListBuilderClassAdapter(chain);
         chain = new BraveDropdownItemViewInfoListManagerClassAdapter(chain);
