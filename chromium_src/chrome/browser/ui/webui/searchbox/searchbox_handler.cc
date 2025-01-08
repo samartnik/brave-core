baa1deb36afef3a8e2ec6fff55a84f44fad5056d
mkarolin <max@brave.com>
2025-01-07 19:05:24 -0500

    [cr133] Some searchbox functionality moved.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/5d75c527e312d
    
    commit 5d75c527e312d3c18915498cce640b876a4ec9aa
    Author: Nihar Majmudar <niharm@google.com>
    Date:   Thu Jan 2 07:27:32 2025 -0800
    
        [searchbox] Move generic methods up to SearchboxHandler.
    
        No-op change. Right now a lot of generic searchbox methods (methods that
        all searchboxes need) are implemented in RealboxHandler. We want to move
        these up to the base SearchboxHandler class so other handlers can
        implement handler specific methods.
    
        Follow ups plan to move lens specific code out of the RealboxHandler.
    
        Bug: 386819657

--- chromium_src/chrome/browser/ui/webui/searchbox/searchbox_handler.cc
+++ chromium_src/chrome/browser/ui/webui/searchbox/searchbox_handler.cc
@@ -3,24 +3,19 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "brave/components/vector_icons/vector_icons.h"
-#include "components/omnibox/browser/vector_icons.h"
+#include "chrome/browser/ui/webui/searchbox/searchbox_handler.h"
 
-namespace {
-constexpr char kLeoWindowTabNewIconResourceName[] =
-    "//resources/brave-icons/window-tab-new.svg";
-}  // namespace
+#include "components/omnibox/browser/autocomplete_input.h"
 
-// This patches |ActionVectorIconToResourceName| to include the additional
-// omnibox icons we add in Brave. Unfortunately, the method is used inside the
-// file, so we can't just override the method. There is only one usage of
-// kSwitchIcon in the file, so we add additional statements from it.
-#define kSwitchIcon kSwitchIcon.name) {}        \
-  if (icon.name == kLeoWindowTabNewIcon.name) { \
-    return kLeoWindowTabNewIconResourceName;    \
-  }                                             \
-  if (icon.name == omnibox::kSwitchIcon
+// We tweak a few AutocompleteInput settings because unlike Chromium we only
+// want keyword search results.
+#define set_prefer_keyword(prefer)                    \
+  set_keyword_mode_entry_method(                      \
+      metrics::OmniboxEventProto::KEYBOARD_SHORTCUT); \
+  autocomplete_input.set_prefer_keyword(true)
+#define set_allow_exact_keyword_match(allow) set_allow_exact_keyword_match(true)
 
 #include "src/chrome/browser/ui/webui/searchbox/searchbox_handler.cc"
 
-#undef kSwitchIcon
+#undef set_prefer_keyword
+#undef set_allow_exact_keyword_match
