06836784a9986549055a1553bc6c23b4187865ad
Max Karolinskiy <max@brave.com>
2025-01-21 14:08:13 -0500

    fixup! [cr133] Some searchbox functionality moved.

--- chromium_src/chrome/browser/ui/webui/searchbox/searchbox_handler.cc
+++ chromium_src/chrome/browser/ui/webui/searchbox/searchbox_handler.cc
@@ -5,7 +5,24 @@
 
 #include "chrome/browser/ui/webui/searchbox/searchbox_handler.h"
 
+#include "brave/components/vector_icons/vector_icons.h"
 #include "components/omnibox/browser/autocomplete_input.h"
+#include "components/omnibox/browser/vector_icons.h"
+
+namespace {
+constexpr char kLeoWindowTabNewIconResourceName[] =
+    "//resources/brave-icons/window-tab-new.svg";
+}  // namespace
+
+// This patches |ActionVectorIconToResourceName| to include the additional
+// omnibox icons we add in Brave. Unfortunately, the method is used inside the
+// file, so we can't just override the method. There is only one usage of
+// kSwitchIcon in the file, so we add additional statements from it.
+#define kSwitchIcon kSwitchIcon.name) {}        \
+  if (icon.name == kLeoWindowTabNewIcon.name) { \
+    return kLeoWindowTabNewIconResourceName;    \
+  }                                             \
+  if (icon.name == omnibox::kSwitchIcon
 
 // We tweak a few AutocompleteInput settings because unlike Chromium we only
 // want keyword search results.
@@ -19,3 +36,4 @@
 
 #undef set_prefer_keyword
 #undef set_allow_exact_keyword_match
+#undef kSwitchIcon
