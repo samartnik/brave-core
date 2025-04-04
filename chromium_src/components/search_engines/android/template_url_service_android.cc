6a7321d11ce639a93e5277596c20560121d50efc
deeppandya <deeppandya91@gmail.com>
2025-04-04 00:05:23 -0400

    Add bytecode tests
    Update strings path

--- chromium_src/components/search_engines/android/template_url_service_android.cc
+++ chromium_src/components/search_engines/android/template_url_service_android.cc
@@ -31,8 +31,6 @@ jboolean TemplateUrlServiceAndroid::AddSearchEngine(
     const base::android::JavaParamRef<jstring>& search_engine_title,
     const base::android::JavaParamRef<jstring>& search_engine_keyword,
     const base::android::JavaParamRef<jstring>& search_engine_url) {
-  LOG(ERROR) << "brave_search : "
-             << "TemplateUrlServiceAndroid::AddSearchEngine";
   const TemplateURL* existing = template_url_service_->GetTemplateURLForKeyword(
       base::android::ConvertJavaStringToUTF16(env, search_engine_keyword));
   if (existing) {
@@ -79,11 +77,7 @@ void TemplateUrlServiceAndroid::RemoveSearchEngine(
     const base::android::JavaParamRef<jstring>& search_engine_keyword) {
   const TemplateURL* existing = template_url_service_->GetTemplateURLForKeyword(
       base::android::ConvertJavaStringToUTF16(env, search_engine_keyword));
-  LOG(ERROR) << "brave_search : "
-             << "TemplateUrlServiceAndroid::RemoveSearchEngine 1";
   if (existing) {
-    LOG(ERROR) << "brave_search : "
-               << "TemplateUrlServiceAndroid::RemoveSearchEngine 2";
     template_url_service_->Remove(existing);
   }
 }
