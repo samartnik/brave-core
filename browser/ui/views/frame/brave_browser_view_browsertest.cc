9dacae1ec853a7da87c9cbbad9ee9794dafd32dc
Claudio DeSouza <cdesouza@brave.com>
2026-05-01 10:50:22 +0100

    [WIP][cr149] `SidePanel` using toolbar side panel height
    This change drops the use of `contents_height_side_panel_` in favour of
    `toolbar_height_side_panel_`. It all changes our interface for
    `SidePanel` to match the changes in upstream for how `SidePanelType` is
    passed around.
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/32daa3e6986fa59dc7adeca3104b17b0a0b63f63
    
    commit 32daa3e6986fa59dc7adeca3104b17b0a0b63f63
    Author: Caroline Rising <corising@chromium.org>
    Date:   Wed Apr 29 11:37:34 2026 -0700
    
        [Side panel] Make all side panels use the toolbar height side panel view.
    
        This removes usage of the content height side panel view and makes the
        toolbar height side panel layout according to the current feature's
        SidePanelType. This also removes all no unnecessary uses of
        SidePanelType as a param for various side panel accessors.
    
        In a followup we will rename the toolbar height side panel.
    
        Bug: 505775465, 505776523
        Change-Id: I5fd80c00064621250619cf4b3c9df326f4bc2ea7
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7789262
        Commit-Queue: Caroline Rising <corising@chromium.org>
        Reviewed-by: Dana Fried <dfried@chromium.org>
        Reviewed-by: Eshwar Stalin <estalin@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1622601}

--- browser/ui/views/frame/brave_browser_view_browsertest.cc
+++ browser/ui/views/frame/brave_browser_view_browsertest.cc
@@ -340,7 +340,7 @@ IN_PROC_BROWSER_TEST_P(BraveBrowserViewWithRoundedCornersTest,
   RunScheduledLayouts();
 
   views::View* contents_container = browser_view()->contents_container();
-  views::View* side_panel = browser_view()->contents_height_side_panel();
+  views::View* side_panel = browser_view()->toolbar_height_side_panel();
   const auto rounded_corners_margin = BraveContentsViewUtil::kMarginThickness;
   const auto rounded_corners_border_radius = GetRoundedCornersBorderRadius();
   const auto rounded_corners_border_radius_at_window_corner =
@@ -407,7 +407,7 @@ IN_PROC_BROWSER_TEST_P(BraveBrowserViewWithRoundedCornersTest,
   RunScheduledLayouts();
 
   views::View* contents_container = browser_view()->contents_container();
-  views::View* side_panel = browser_view()->contents_height_side_panel();
+  views::View* side_panel = browser_view()->toolbar_height_side_panel();
   const auto rounded_corners_margin = BraveContentsViewUtil::kMarginThickness;
   const auto rounded_corners_border_radius = GetRoundedCornersBorderRadius();
   const auto rounded_corners_border_radius_at_window_corner =
@@ -448,7 +448,7 @@ IN_PROC_BROWSER_TEST_P(BraveBrowserViewWithRoundedCornersTest,
   RunScheduledLayouts();
 
   views::View* contents_container = browser_view()->contents_container();
-  views::View* side_panel = browser_view()->contents_height_side_panel();
+  views::View* side_panel = browser_view()->toolbar_height_side_panel();
   const auto rounded_corners_margin = BraveContentsViewUtil::kMarginThickness;
   const auto rounded_corners_border_radius = GetRoundedCornersBorderRadius();
   const auto rounded_corners_border_radius_at_window_corner =
