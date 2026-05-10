7b755e728d2a997612cf3c89c981ed9ad7f959a2
Emerick Rogul <erogul@brave.com>
2026-05-10 18:05:50 -0400

    Revert "[cr149] Use WaitUntilInvisible to avoid flakes in vertical tab tests testing for widget visibility"
    This reverts commit 3b5111b3bacb10dd79ae32d25a4545c1c11d9487.

--- browser/ui/views/frame/vertical_tabs/vertical_tab_strip_browsertest.cc
+++ browser/ui/views/frame/vertical_tabs/vertical_tab_strip_browsertest.cc
@@ -65,7 +65,6 @@
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/layout_manager.h"
 #include "ui/views/test/views_test_utils.h"
-#include "ui/views/test/widget_test.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "chrome/browser/ui/view_ids.h"
@@ -2182,7 +2181,6 @@ IN_PROC_BROWSER_TEST_P(VerticalTabStripHideCompletelyTest,
   EXPECT_EQ(BraveVerticalTabStripRegionView::State::kCollapsed,
             region_view->state());
   if (!base::FeatureList::IsEnabled(tabs::kBraveVerticalTabStripEmbedded)) {
-    views::test::WidgetVisibleWaiter(vertical_tab_widget).WaitUntilInvisible();
     EXPECT_FALSE(vertical_tab_widget->IsVisible());
   }
   EXPECT_FALSE(region_view->GetVisible());
@@ -2194,7 +2192,6 @@ IN_PROC_BROWSER_TEST_P(VerticalTabStripHideCompletelyTest,
   screen->SetCursorScreenPointForTesting(mouse_position);
   browser_view()->HandleBrowserWindowMouseEvent(GetDummyEvent());
   if (!base::FeatureList::IsEnabled(tabs::kBraveVerticalTabStripEmbedded)) {
-    views::test::WidgetVisibleWaiter(vertical_tab_widget).WaitUntilInvisible();
     EXPECT_FALSE(vertical_tab_widget->IsVisible());
   }
   EXPECT_FALSE(region_view->GetVisible());
